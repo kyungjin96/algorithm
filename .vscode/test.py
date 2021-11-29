274d31bc7b269fa3ba96f2b4e929411fc1520e83
kyungjin96 <wjsrudwls96@naver.com>
2021-11-29 17:31:56 +0900

    따시

--- .vscode/test.py
+++ .vscode/test.py
@@ -3,16 +3,17 @@ def func(x,y):
     z = x+y
     return z 
 
-def test_a(x,y):
-    z = x * y
-    k = x / y
+def test_a(xx,yy):
+    z = xx * yy
+    k = xx / yy
     return z , k
     
-
 if __name__ == '__main__':
+    # xx = 10
+    # yy = 2
     try:
         func(50,20)
-        test_a(10*0)
+        test_a(10,2)
         pass
     except Exception as e:
         print('typeError')
