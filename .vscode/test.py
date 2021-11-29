177ab70e024b6d9707059729f65895ffdf8192e5
kyungjin96 <wjsrudwls96@naver.com>
2021-11-29 17:40:00 +0900

    따시

--- .vscode/test.py
+++ .vscode/test.py
@@ -9,12 +9,11 @@ def test_a(xx,yy):
     return z , k
     
 if __name__ == '__main__':
-    # xx = 10
-    # yy = 2
+
     try:
         func(50,20)
         test_a(10,2)
         pass
     except Exception as e:
-        print('typeError')
+        print('따시')
         pass
