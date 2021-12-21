95c036392d3f78b7bc1c1dbfe5edecf4e3a25e5b
kyungjin96 <wjsrudwls96@naver.com>
2021-12-21 10:41:28 +0900

    커밋

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # algorithm
 
 ## made by kj
+### 알고리즘 기본 연습 : test_algorithm.py
--- math_fun.py
+++ math_fun.py
@@ -1,7 +1,13 @@
 import numpy as np
 
-laser = [0.1, 0.2, 0.3, 0.4, 0.5]
+# laser = [0.1, 0.2, 0.3, 0.4, 0.5]
 
-laser_arr = np.array(laser)
-result = np.count_nonzero(laser_arr >= 0.2)
-print(result)
+# laser_arr = np.array(laser)
+# result = np.count_nonzero(laser_arr >= 0.2)
+import math
+
+radian = math.radians(90)
+
+con_d = math.degrees(radian)
+
+print(radian,':::',con_d)
